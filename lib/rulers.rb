b74c3d88bd5126da291b8f640a7c4b1c9ae166ef
Emily Giurleo <e.m.giurleo@gmail.com>
2021-11-15 10:27:33 -0500

    Chapter 2, Exercise 2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -16,7 +16,17 @@ module Rulers
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
+
+      begin
+        text = controller.send(act)
+      rescue StandardError => e
+        return [
+          500,
+          {'Content-Type' => 'text/html'},
+          [e.message]
+        ]
+      end
+
       [200, {'Content-Type' => 'text/html'},
         [text]]
     end
